3363a9590b5e9b2f22f1a6e37413c70bfa04deb4
Torrex1 <flendik666@gmail.com>
2024-12-02 00:09:20 +0300

    create CRUD for user's feedback

---
+++ backend/controller/feedback.contoller.js
@@ -0,0 +1,34 @@
+import db from "../database/db.js";
+
+class FeedbackController {
+    async createFeedback (req, res) {
+        const { rating, context, userId } = req.body;
+        const feedback = await db.query('INSERT INTO feedback (rating, context, user_id) VALUES ($1, $2, $3) RETURNING *',
+            [rating, context, userId]
+        )
+
+        res.json(feedback.rows[0]);
+    }
+    async getUsersFeedback (req, res) {
+        const id = req.query.id;
+        const feedback = await db.query('SELECT * from feedback where user_id = $1', [id]);
+
+        res.json(feedback.rows);
+    }
+    async updateFeedback (req, res) {
+        const feedback = await db.query('UPDATE feedback set rating = $1, context = $2 where id = $3', 
+            [req.body.rating, req.body.context, req.params.id]
+        )
+
+        res.json(feedback.rows[0]);
+    }
+    async deleteFeedback (req, res) {
+        const feedback = await db.query('DELETE from feedback where id = $1', 
+            [req.params.id]
+        )
+
+        res.json('Feedback deleted');
+    }
+}
+
+export default new FeedbackController();
