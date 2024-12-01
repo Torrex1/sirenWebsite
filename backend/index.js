682c2dfa8990e9509aec6b66925e58491b8a7739
Torrex1 <flendik666@gmail.com>
2024-12-01 22:55:25 +0300

    connect DB and add user router

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,27 @@
 import express from "express";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+import db from "./database/db.js";
+
 dotenv.config();
 
 const PORT = process.env.PORT || 5000
-
 const app = express();
 
-app.listen(PORT, () => {
-    console.log('Server started on ' + `http://localhost:${PORT}`);
-});
+app.use(express.json());
+app.use('/api', userRouter);
+
+const start = async () => {
+    try {
+        await db.connect().then(() => console.log('DB connected'));
+        app.listen(PORT, () => {
+            console.log('Server started on ' + `http://localhost:${PORT}`);
+        });
+    }
+
+    catch (e) {
+        console.log(`DB error ${e}`);
+    }
+    
+}
+start();
