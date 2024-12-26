73f21d248cd19ff62d6f7560d795510044a11755
Torrex1 <flendik666@gmail.com>
2024-12-26 15:47:36 +0300

    use tokens generate func and insert refresh token in db for registration

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -1,13 +1,43 @@
 import db from "../database/db.js";
+import bcrypt from 'bcrypt';
+
+import tokenService from '../utils/jwt.js'
 
 class UserController {
-    async createUser (req, res) {
-        const { username, email, password } = req.body;
-        const user = await db.query('INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING *',
-            [username, email, password]
+    async registration (req, res) {
+        const {username, email, password} = req.body;
+
+        const candidate = await db.query('SELECT email FROM users WHERE email = $1',
+            [email]
         )
 
-        res.json(user.rows[0]); 
+        if (candidate.rows.length > 0) {
+            return res.status(400).json({
+                message: `Пользователь с почтой ${email} уже существует!`
+            });
+        }
+
+        const hashPassword = await bcrypt.hash(password, 10);
+        const user = await db.query('INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING *',
+            [username, email, hashPassword]
+        )
+
+        // генерация токена
+
+        const tokens = tokenService.generateToken({id: user.rows[0].id});
+
+        // сохранение refresh Токена в БД
+        await db.query('INSERT INTO token (userId, refreshToken) VALUES($1, $2) RETURNING *',
+            [user.rows[0].id, tokens.refresh_token]);
+
+        res
+            .cookie('refreshToken', tokens.refresh_token, { httpOnly: true })
+            .json({ user: user.rows[0], access_token: tokens.refresh_token});
+
+    }
+
+    async login (req, res) {
+
     }
 
     async getUsers (req, res) {
