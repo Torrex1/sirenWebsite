21eb7837fc7505df0bccca5660d3ba2e8c4dd619
Torrex1 <flendik666@gmail.com>
2024-12-28 01:05:52 +0300

    add new route for refresh access token

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -2,11 +2,13 @@ import { Router } from "express";
 const router = Router();
 
 import UserController from "../controller/user.controller.js";
+import authMiddleware from "../middlewares/authMiddleware.js";
 
 router.post('/registration', UserController.registration);
 router.post('/login', UserController.login);
 router.post('/logout', UserController.logout);
-router.get('/users', UserController.getUsers);
+router.post('/refresh', UserController.refresh)
+router.get('/users', authMiddleware ,UserController.getUsers);
 router.get('/user/:id', UserController.getOneUser);
 router.put('/user/:id', UserController.updateUser);
 router.delete('/user/:id', UserController.deleteUser);
