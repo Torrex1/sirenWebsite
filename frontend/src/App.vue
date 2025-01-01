522c3d51222cee548d6c27a96eeba3d87b524783
Torrex1 <flendik666@gmail.com>
2025-01-01 22:39:59 +0300

    add Vue3.js in project

---
+++ frontend/src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <Header/>
+  <Main/>
+</template>
+
+<script setup>
+  import Header from './components/header-components/Header.vue'
+  import Main from './components/main-components/Main.vue'
+</script>
