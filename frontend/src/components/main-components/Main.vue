4885950e11ed667a2efeba9248532a03a3c8e172
Torrex1 <flendik666@gmail.com>
2025-01-03 01:22:23 +0300

    add card list with card in main page

---
+++ frontend/src/components/main-components/Main.vue
@@ -0,0 +1,34 @@
+<script setup>
+  import CardList from "./CardList.vue";
+</script>
+
+<template>
+  <div class="wrapper">
+    <div class="main-container">
+      <h1>New Drops</h1>
+
+      <CardList/>
+    </div>
+  </div>
+
+</template>
+
+<style scoped>
+
+.wrapper {
+    display: flex;
+    justify-content: center;
+    width: 1280px;
+    height: 100vh;
+    margin: auto;
+    background-color: #ffffff;
+    border-radius: 10px;
+  }
+
+  h1 {
+    color: var(--main-title-color);
+    font-size: 3.5rem;
+    margin-bottom: 1rem;
+    padding-top: 20px;
+  }
+</style>
