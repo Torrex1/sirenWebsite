615115dcf6e74090dde893eea2677450ee3134df
Torrex1 <flendik666@gmail.com>
2025-01-03 01:21:55 +0300

    create card list component

---
+++ frontend/src/components/main-components/CardList.vue
@@ -0,0 +1,18 @@
+<script setup>
+  import Card from "./Card.vue";
+</script>
+
+<template>
+  <div class="cards-container">
+    <Card/>
+  </div>
+</template>
+
+<style scoped>
+  .cards-container {
+    display: grid;
+    grid-template-columns: repeat(4, minmax(0, 1fr));
+    justify-content: center;
+    gap: 50px;
+  }
+</style>
