81458652cab7100bedc72557908bfa9767aed3a9
Torrex1 <flendik666@gmail.com>
2025-01-03 03:12:13 +0300

    creating an order history page for a user profile

---
+++ frontend/src/components/profile-components/OrderHistory.vue
@@ -0,0 +1,36 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="wrapper">
+    <h1>ORDER HISTORY</h1>
+    <span>You haven't placed any orders yet.</span>
+    <button>BACK TO SHOPPING</button>
+  </div>
+
+</template>
+
+<style scoped>
+  .wrapper {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+  }
+  button {
+    cursor: pointer;
+    padding: 15px;
+    width: 25%;
+    border-radius: 10px;
+    border: none;
+    background-color: rgba(200, 60, 225, 0.89);
+    color: white;
+  }
+  button:hover {
+    background-color: rgba(171, 51, 193, 0.89);
+  }
+
+  span {
+    font-size: 18px;
+  }
+</style>
