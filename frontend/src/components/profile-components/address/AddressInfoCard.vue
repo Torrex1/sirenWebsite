a69cab56a4e8df2d653b33c1eda6d1149e5d452f
Torrex1 <flendik666@gmail.com>
2025-01-10 01:34:53 +0300

    create delete address card func

--- frontend/src/components/profile-components/address/AddressInfoCard.vue
+++ frontend/src/components/profile-components/address/AddressInfoCard.vue
@@ -1,22 +1,43 @@
 <script setup>
+  import axios from "axios";
+
   const props = defineProps({
     id: Number,
-    firstName: String,
-    lastName: String,
+    first_name: String,
+    last_name: String,
     city: String,
     address: String,
     zipcode: Number,
     country: String,
-  })
+  });
+  const emit = defineEmits(['fetchAddressBooks']);
+
+  const deleteAddress = async () => {
+    try {
+      await axios.delete(`http://localhost:3000/api/address/${props.id}`).then((response) => {
+        console.log(response, "Все удалил!");
+        emit("fetchAddressBooks");
+      })
+    }
+    catch (error) {
+      console.log(error)
+    }
+  }
 </script>
 
 <template>
   <div class="address-info">
-    <span class="full-name">{{ firstName }} {{ lastName }},</span> <br>
+    <span class="full-name">{{ first_name }} {{ last_name }},</span> <br>
     <span class="city">{{ city }}</span> <br>
     <span class="address">{{ address }}, </span>
     <span class="zipcode">{{ zipcode }}</span> <br>
     <span class="country">{{ country }}</span>
+
+    <div class="button-list">
+      <button>EDIT</button>
+      <button @click="deleteAddress()">DELETE</button>
+    </div>
+
   </div>
 </template>
 
@@ -27,5 +48,23 @@
 }
 .address-info span {
   font-size: 16px;
+}
+
+
+.button-list {
+  display: flex;
+  gap: 1rem;
+  margin-top: 20px;
+}
+
+.button-list button {
+  border: none;
+  cursor: pointer;
+  text-decoration: underline;
+  opacity: 0.8;
+}
+
+.button-list button:hover {
+  opacity: 1;
 }
 </style>
