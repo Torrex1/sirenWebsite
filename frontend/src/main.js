3a75c97d83a420d7338a33432487457f8b47f9c5
Torrex1 <flendik666@gmail.com>
2025-01-11 00:19:30 +0300

    init vue router in main proj js file

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,20 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createRouter, createWebHistory } from "vue-router";
+import routes from './routes.js'
 
 import './style.css'
 import App from './App.vue'
+import * as path from "node:path";
 
 const pinia = createPinia();
 const app = createApp(App);
 
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+})
+
 app.use(pinia);
+app.use(router);
 app.mount('#app')
