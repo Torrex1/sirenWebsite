c851a747a189d063f49ab7ffc77a8ad68397d191
Torrex1 <flendik666@gmail.com>
2025-01-11 00:18:54 +0300

    create new main page component

---
+++ frontend/src/components/MainPage.vue
@@ -0,0 +1,13 @@
+<script setup>
+  import Header from "./header-components/Header.vue";
+  import MainProfilePage from "./profile-components/MainProfilePage.vue";
+</script>
+
+<template>
+  <Header/>
+  <MainProfilePage/>
+</template>
+
+<style scoped>
+
+</style>
