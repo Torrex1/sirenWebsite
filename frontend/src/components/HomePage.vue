879d667caf75cf3ed7c05857359a6dd4c9f22104
Torrex1 <flendik666@gmail.com>
2025-01-14 00:32:11 +0300

    create home page

---
+++ frontend/src/components/HomePage.vue
@@ -0,0 +1,13 @@
+<script setup>
+  import Main from "./main-components/Main.vue";
+</script>
+
+<template>
+  <div style="margin-top: 20px">
+    <Main />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
