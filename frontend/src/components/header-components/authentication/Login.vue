c4ad94f2261dd17f6e7df015c5c00b4e72da750c
Torrex1 <flendik666@gmail.com>
2025-01-28 18:28:21 +0300

    login's layout refactoring

--- frontend/src/components/header-components/authentication/Login.vue
+++ frontend/src/components/header-components/authentication/Login.vue
@@ -3,57 +3,37 @@
 </script>
 
 <template>
-  <div class="modal-shadow" style="border: 1px solid red;">
-    <div class="modal-wrapper">
-      <form class="login-form">
-        <label>
-          <span>Email</span> <br>
-          <input type="email" PLACEHOLDER="Email" />
-        </label>
+  <div class="modal-wrapper">
+    <form class="login-form">
+      <h1>WELCOME BACK</h1>
+      <label>
+        <span>Email</span> <br>
+        <input type="email" PLACEHOLDER="Email" />
+      </label>
 
-        <label>
-          <span>Password</span> <br>
-          <input type="password" PLACEHOLDER="Пароль" />
-        </label>
+      <label>
+        <span>Password</span> <br>
+        <input type="password" PLACEHOLDER="Пароль" />
+      </label>
 
-        <span>Еще нет аккаунта?
-          <a>
-            <RouterLink style="color: rgba(97,198,26,0.7); font-weight: normal" to="/register">Зарегестрируйся</RouterLink>
-          </a>
-        </span>
+      <span>Еще нет аккаунта?
+        <a>
+          <RouterLink style="color: rgba(97,198,26,0.7); font-weight: normal" to="/register">Зарегестрируйся</RouterLink>
+        </a>
+      </span>
 
-        <button>Войти</button>
-
-      </form>
-    </div>
+      <button>Войти</button>
+    </form>
   </div>
 </template>
 
 <style scoped>
-
-  .modal-shadow {
-    position: fixed;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    z-index: 10;
-    background-color: rgba(0, 0, 0, 0.2);
-
+  .modal-wrapper {
+    background-color: #f1f2f3;
     display: flex;
     justify-content: center;
     align-items: center;
-  }
-
-  .modal-wrapper {
-    position: fixed;
-    border: 2px solid rgba(0, 0, 0, 0.13);
-    background-color: #ffffff;
-    width: 30%;
-    height: 70%;
-    z-index: 20;
-
-    border-radius: 10px;
+    height: 80vh;
   }
 
   .login-form {
@@ -61,10 +41,11 @@
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    height: 100%;
+    padding: 50px;
+    border-radius: 10px;
     gap: 30px;
+    background-color: #ffffff;
   }
-
 
   .login-form input {
     padding: 14px;
@@ -77,7 +58,6 @@
 
   button {
     padding: 10px;
-    width: 100px;
     border-radius: 10px;
     cursor: pointer;
     border: none;
@@ -94,5 +74,4 @@
     outline: none;
     border-color: #61c61a;
   }
-
 </style>
