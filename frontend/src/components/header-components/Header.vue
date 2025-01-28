f0c38a03ec6b032f9bab975f8520e90445bf74ef
Torrex1 <flendik666@gmail.com>
2025-01-28 18:26:17 +0300

    add auth actions

--- frontend/src/components/header-components/Header.vue
+++ frontend/src/components/header-components/Header.vue
@@ -1,5 +1,7 @@
 <script setup>
+  import { useAuthStore } from "../../stores/authStore.js";
 
+  const authStore = useAuthStore();
 </script>
 
 <template>
@@ -17,18 +19,36 @@
       </ul>
 
       <ul class="logo">
-        <img src="../../assets/icons/logo.svg" alt="logo">
+        <RouterLink to="/">
+          <img src="../../assets/icons/logo.svg" alt="logo">
+        </RouterLink>
       </ul>
 
-      <ul class="icon-list">
+      <ul class="icon-list" v-show="authStore.isAuthenticated">
         <li class="favorite-icon">
           <img src="../../assets/icons/heart.svg" alt="search">
         </li>
         <li class="cart-icon">
           <img src="../../assets/icons/cart.svg" alt="cart">
         </li>
-        <li class="user-icon">
-          <img src="../../assets/icons/user.svg" alt="user">
+        <li class="user-icon" >
+          <RouterLink to="/profile">
+            <img src="../../assets/icons/user.svg" alt="user">
+          </RouterLink>
+        </li>
+      </ul>
+
+      <ul class="auth-actions" v-show="!authStore.isAuthenticated">
+        <li>
+          <RouterLink to="/login">
+            <button>Log in</button>
+          </RouterLink>
+        </li>
+
+        <li>
+          <RouterLink to="/register">
+            <button>Sign up</button>
+          </RouterLink>
         </li>
       </ul>
 
@@ -37,6 +57,21 @@
 </template>
 
 <style scoped>
+  .auth-actions {
+    display: flex;
+    gap: 1rem;
+  }
+  .auth-actions button {
+    border: 1px solid transparent;
+    padding: 10px;
+    cursor: pointer;
+    overflow: hidden;
+    transition: 0.3s ease;
+  }
+  .auth-actions button:hover {
+    border-color: #61c61a;
+    border-radius: 3px;
+  }
 
   .menu li, .icon-list img, .logo img {
     cursor: pointer;
