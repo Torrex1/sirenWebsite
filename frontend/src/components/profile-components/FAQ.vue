397f6b4e84198c2053fd3e5650e3d01604d9afce
Torrex1 <flendik666@gmail.com>
2025-01-28 18:27:58 +0300

    add new options in faq obj for modal logic

--- frontend/src/components/profile-components/FAQ.vue
+++ frontend/src/components/profile-components/FAQ.vue
@@ -1,8 +1,6 @@
 <script setup>
   import {ref} from "vue";
 
-  // let isIconPressed = ref(false);
-
   const openItems = ref({});
 
   const toggleText = (key) => {
@@ -17,63 +15,41 @@
     second: {
       title: "Сколько осуществляется доставка?",
       description: "Сроки доставки зависят от удаленности Вашего города. Как правильно доставка занимает около 4-5 дней.",
-    }
+    },
+    third: {
+      title: "Почему не доставляете в другие страны?",
+      description: "На данный момент у нас нет возможности реализовать пободное. В будущем мы планируем расширять зону доставки наших товаров. Следите за обновлениями, подписавшись на рассылку.",
+    },
+    fourth: {
+      title: "Сколько стоит доставка?",
+      description: "Доставку уже входит в стоимость товаров, так что Вам не о чем переживать!",
+    },
   }
 </script>
 
 <template>
   <h1>FAQ</h1>
-  <ul>
+
+  <ul class="faq-wrapper">
     <li v-for="(item, index) in faqObj">
       <div @click="toggleText(index)" style="display: flex; gap: 5px; cursor: pointer;">
-        <h4>{{ item.title }}</h4>
+        <h3>{{ item.title }}</h3>
         <img src="../../assets/icons/down-arrow.svg" alt="down-arrow">
       </div>
       <span v-show="openItems[index]">{{ item.description }}</span>
     </li>
-
-<!--    <li>-->
-<!--      <div @click="showText()" style="cursor: pointer;">-->
-<!--        <span style="font-size: 18px;">Куда доставляется доставка?</span>-->
-<!--      </div>-->
-
-<!--      <span v-show="isIconPressed" style="font-size: 16px;">На данный момент доставка осуществляется только на территории РФ через СДЭК</span>-->
-<!--    </li>-->
-
-<!--    <li>-->
-<!--      <div @click="showText()" style="cursor: pointer;">-->
-<!--        <span style="font-size: 18px;">Сколько осуществляется доставка?</span>-->
-<!--        <img src="../../assets/icons/down-arrow.svg" alt="down-arrow">-->
-<!--      </div>-->
-<!--      <span v-show="isIconPressed" style="font-size: 16px;">Сроки доставки зависят от удаленности Вашего города. Как правильно доставка занимает около 4-5 дней.</span>-->
-<!--    </li>-->
-
-<!--    <li>-->
-<!--      <div @click="showText()" style="cursor: pointer;">-->
-<!--        <span style="font-size: 18px;">Почему не доставляете в другие страны?</span>-->
-<!--        <img src="../../assets/icons/down-arrow.svg" alt="down-arrow">-->
-<!--      </div>-->
-<!--      <span v-show="isIconPressed" style="font-size: 16px; opacity: 0.8">На данный момент у нас нет возможности реализовать пободное. В будущем мы планируем расширять зону доставки наших товаров. Следите за обновлениями, подписавшись на рассылку.</span>-->
-<!--    </li>-->
-
-<!--    <li>-->
-<!--      <div @click="showText()" style="cursor: pointer;">-->
-<!--        <span style="font-size: 18px;">Сколько осуществляется доставка?</span>-->
-<!--        <img src="../../assets/icons/down-arrow.svg" alt="down-arrow">-->
-<!--      </div>-->
-<!--      <span v-show="isIconPressed" style="font-size: 16px;">Сроки доставки зависят от удаленности Вашего города. Как правильно доставка занимает около 4-5 дней.</span>-->
-<!--    </li>-->
-
-<!--    <li>-->
-<!--      <div @click="showText()" style="cursor: pointer;">-->
-<!--        <span style="font-size: 18px;">Сколько стоит доставка?</span>-->
-<!--        <img src="../../assets/icons/down-arrow.svg" alt="down-arrow">-->
-<!--      </div>-->
-<!--      <span v-show="isIconPressed" style="font-size: 16px;">Доставку уже входит в стоимость товаров, так что Вам не о чем переживать!</span>-->
-<!--    </li>-->
   </ul>
 </template>
 
 <style scoped>
+  .faq-wrapper {
+    display: flex;
+    flex-direction: column;
+    margin-top: 1rem;
+    gap: 10px;
+  }
 
+  h3 {
+    font-weight: normal;
+  }
 </style>
