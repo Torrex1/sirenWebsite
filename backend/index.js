acb1b3448c8e7bd27b33dd1834b2edfbc62fb5ff
Torrex1 <flendik666@gmail.com>
2025-02-13 21:51:08 +0300

    add cors options

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,10 @@ const PORT = process.env.PORT || 5000
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+}));
 app.use(cookieParser());
 
 app.use('/api', userRouter);
