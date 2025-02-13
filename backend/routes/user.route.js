8b74d757b4346fcfcf7e60622aa8368032971f5c
Torrex1 <flendik666@gmail.com>
2025-02-13 21:37:57 +0300

    add new route for check user's auth

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -3,14 +3,16 @@ const router = Router();
 
 import UserController from "../controller/user.controller.js";
 import authMiddleware from "../middlewares/authMiddleware.js";
+import userController from "../controller/user.controller.js";
 
 router.post('/registration', UserController.registration);
 router.post('/login', UserController.login);
 router.post('/logout', UserController.logout);
 router.post('/refresh', UserController.refresh)
-router.get('/users', authMiddleware ,UserController.getUsers);
-router.get('/user/:id', UserController.getOneUser);
+router.get('/users', authMiddleware, UserController.getUsers);
+router.get('/user/:id', authMiddleware, UserController.getOneUser);
 router.put('/user/:id', UserController.updateUser);
 router.delete('/user/:id', UserController.deleteUser);
+router.get('/checkAuth', authMiddleware, UserController.checkAuth);
 
 export default router;
