8dbbe577da0c9d78d82afd92e5c4bbc1208bab98
Torrex1 <flendik666@gmail.com>
2025-02-13 21:41:24 +0300

    add fetch request to db for get products

--- frontend/src/components/main-components/CardList.vue
+++ frontend/src/components/main-components/CardList.vue
@@ -1,10 +1,25 @@
 <script setup>
   import Card from "./Card.vue";
+  import axios from 'axios';
+  import { onMounted, ref } from "vue";
+
+  const products = ref([]);
+  const getProducts = async () => {
+    const resp = await axios.get('http://localhost:3000/api/products');
+    products.value = resp.data;
+    console.log(products.value);
+  }
+  onMounted(getProducts);
 </script>
 
 <template>
   <div class="cards-container">
-    <Card/>
+    <Card v-for="product in products"
+          :key="product.id"
+          :title="product.name"
+          :price="product.price"
+    />
+
   </div>
 </template>
 
