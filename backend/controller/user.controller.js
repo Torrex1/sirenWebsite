b7288ae8a05237a76a7bc5cc3d2d593c437f3108
Torrex1 <flendik666@gmail.com>
2025-02-13 21:38:31 +0300

    edit login's req body params

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -35,14 +35,13 @@ class UserController {
     }
 
     async login (req, res) {
-        const { username, password } = req.body;
+        const { email, password } = req.body;
 
-        const user = await db.query('SELECT id, username, password FROM users WHERE username = $1',
-            [username]);
+        const user = await db.query('SELECT id, email, password FROM users WHERE email = $1',
+            [email]);
         if (user.rows.length === 0) {
             return res.status(400).json({message: "Пользователя с таким логином не существует"});
         }
-        console.log(user.rows);
 
         const isPassValid = await bcrypt.compare(password, user.rows[0].password);
         if (!isPassValid) {
@@ -66,7 +65,6 @@ class UserController {
         res
             .cookie('refreshToken', tokens.refresh_token, { httpOnly: true, maxAge: 30 * 24 * 60 * 60 * 1000 })
             .json({ user: user.rows[0], access_token: tokens.access_token, refresh_token: tokens.refresh_token });
-
     }
 
     async logout (req, res) {
@@ -130,6 +128,34 @@ class UserController {
         const user = await db.query('DELETE FROM users WHERE id = $1', [req.params.id]);
         
         res.json('User deleted');
+    }
+
+    // async getAccessTokens (req, res) {
+    //     const token = req.cookies.access_token;
+    //     if (!token) {
+    //         return null;
+    //     }
+    //
+    //     console.log(token);
+    // }
+
+    async checkAuth(req, res) {
+        try {
+            const authHeader = req.headers.authorization;
+            if (!authHeader) {
+                return res.status(401).json({ message: "Нет доступа" });
+            }
+
+            const accessToken = authHeader.split(" ")[1];
+            const userData = tokenService.validateAccessToken(accessToken);
+            if (!userData) {
+                return res.status(401).json({ message: "Токен недействителен" });
+            }
+
+            res.json({ user: userData });
+        } catch (error) {
+            res.status(500).json({ message: "Ошибка авторизации" });
+        }
     }
 }
 
